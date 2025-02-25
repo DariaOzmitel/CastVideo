074b3e4b49635907054901b306e069a5f5734e2b
Daria <vvcxcv@mail.ru>
2025-02-25 18:48:51 +0300

    add uiModule

--- ui/src/main/java/com/example/ui/di/UiModule.kt
+++ ui/src/main/java/com/example/ui/di/UiModule.kt
@@ -1,5 +1,9 @@
 package com.example.ui.di
 
+import com.example.ui.screen.CastVideoScreenViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
-val uiModule = module {}
+val uiModule = module {
+    viewModelOf(::CastVideoScreenViewModel)
+}
